5aed456fa9cdc495e6337acc859592c31b0dcbcf
NayaniShashikala <56906401+NayaniShashikala@users.noreply.github.com>
2020-12-04 19:49:11 +0530

    added profile screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/widgets.dart';
-// import 'package:shop_app/screens/cart/cart_screen.dart';
-// import 'package:shop_app/screens/complete_profile/complete_profile_screen.dart';
-// import 'package:shop_app/screens/details/details_screen.dart';
-// import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
-// import 'package:shop_app/screens/home/home_screen.dart';
-// import 'package:shop_app/screens/login_success/login_success_screen.dart';
-// import 'package:shop_app/screens/otp/otp_screen.dart';
 import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
 import 'package:shop_app/screens/splash/splash_screen.dart';
 
@@ -16,12 +9,5 @@ import 'package:shop_app/screens/splash/splash_screen.dart';
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
   SignInScreen.routeName: (context) => SignInScreen(),
-  // ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
-  // LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
-  // SignUpScreen.routeName: (context) => SignUpScreen(),
-  // CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
-  // OtpScreen.routeName: (context) => OtpScreen(),
-  // HomeScreen.routeName: (context) => HomeScreen(),
-  // DetailsScreen.routeName: (context) => DetailsScreen(),
-  // CartScreen.routeName: (context) => CartScreen(),
+
 };
