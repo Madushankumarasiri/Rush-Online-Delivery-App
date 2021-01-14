9e004174322f3d229a0993f3e1492dba646cdf4b
NayaniShashikala <56906401+NayaniShashikala@users.noreply.github.com>
2021-01-14 19:20:45 +0530

    updated help_center.dart
    added route name

--- lib/screens/help_center.dart
+++ lib/screens/help_center.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 
 class HelpCenter extends StatelessWidget {
+  static const routeName = '/help-center';
   @override
   Widget build(BuildContext context) {
     return Container(
