dec6c6c3125058304eed69df2b11d1eb8edf2321
NayaniShashikala <56906401+NayaniShashikala@users.noreply.github.com>
2021-01-14 19:05:08 +0530

    updated constant.dart file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF64A1FF);
-const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryLightColor = Color(0xFFFFECDF);const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+const kSecondaryColor = Color(0xFF979797);
+const kSecondaryTextColor = Color(0xFF757575);
+// const kTextColor = Color(0xFF757575);
+const kTextColor = Colors.black;
+const kAnimationDuration = Duration(milliseconds: 200);
