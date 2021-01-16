7d6c4b3b199ad96501d6de50c33cfc5f4e0f68a7
Sithum Ranabahu <52326583+sithumranabahu@users.noreply.github.com>
2021-01-16 13:39:35 +0530

    Updated_AuthScreen.dart

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -3,10 +3,23 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 // import 'package:flutter_complete_guide/widgets/auth_card.dart';
 
+enum AuthMode { Signup, Login }
+
 class AuthScreen extends StatelessWidget {
   static const routeName = '/auth';
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    final deviceSize = MediaQuery.of(context).size;
+    return SafeArea(
+        child: Scaffold(
+            body: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Stack(
+                  children: [
+                    SingleChildScrollView(
+                      child: Column(),
+                    )
+                  ],
+                ))));
   }
 }
