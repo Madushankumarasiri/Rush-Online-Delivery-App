ade8ec8109c7c80db8551504002c0403fdee41d2
chanikalakmini <57705660+chanikalakmini@users.noreply.github.com>
2021-01-16 11:10:27 +0530

    updated product_detail_screen.dart

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -10,7 +10,17 @@ class ProductDetailScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
    
-   return Scaffold();
+   final productId =
+        ModalRoute.of(context).settings.arguments as String; // is the id!
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
   }
 
 
