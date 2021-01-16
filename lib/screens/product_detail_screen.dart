162ace230243e7638d9cde1d2bd75031b892e21f
chanikalakmini <57705660+chanikalakmini@users.noreply.github.com>
2021-01-16 11:02:18 +0530

    added product_detail_screen.dart

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+
+class ProductDetailScreen extends StatelessWidget{
+  static const routeName = '/product-detail';
+  @override
+  Widget build(BuildContext context) {
+   
+   return Scaffold();
+  }
+
+
+}
