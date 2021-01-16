8e1601fa8a0c492d17dd69ed1e2ed659061a1877
Sithum Ranabahu <52326583+sithumranabahu@users.noreply.github.com>
2021-01-16 13:32:33 +0530

    Added_AuthScreen.dart

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,12 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
+// import 'package:flutter_complete_guide/widgets/auth_card.dart';
+
+class AuthScreen extends StatelessWidget {
+  static const routeName = '/auth';
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold());
+  }
+}
